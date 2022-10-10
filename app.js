3e1625e8bf6317723398a636fde6487f65f5c82f
Ekanem Ekanem <ekanem.programmer@gmail.com>
2022-10-10 02:58:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('The greates gift gift of man is tomorrow, A new day gives birth to new hope. Hope is the fuel for human survival'));
+app.get('/', (req, res) => res.send('The greates gift gift of man is tomorrow, A new day gives birth to new hope. Hope is the fuel for human survival.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
